0d63b34d58f825c9fb896e19ad0c53db5f2f4749
Davidgtz28 <80834@alumnos.utleon.edu.mx>
2024-12-02 22:10:53 -0600

    Me quiero suicidar

--- src/main/java/com/cinePremier/dulceria/CRUD_Dulceria/CrudDulceriaApplication.java
+++ src/main/java/com/cinePremier/dulceria/CRUD_Dulceria/CrudDulceriaApplication.java
@@ -2,12 +2,30 @@ package com.cinePremier.dulceria.CRUD_Dulceria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudDulceriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDulceriaApplication.class, args);
+	}
+        
+        
+        @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Permitir todos los orígenes, métodos y encabezados
+				registry.addMapping("/")
+						.allowedOrigins("*") // Permitir todos los orígenes
+						.allowedMethods("*") // Permitir todos los métodos (GET, POST, etc.)
+						.allowedHeaders("*"); // Permitir todos los encabezados
+			}
+		};
 	}
 
 }
--- src/main/java/com/cinePremier/dulceria/CRUD_Dulceria/WebConfig.java
+++ src/main/java/com/cinePremier/dulceria/CRUD_Dulceria/WebConfig.java
@@ -1,5 +1,6 @@
 package com.cinePremier.dulceria.CRUD_Dulceria;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,17 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				// Permitir todos los orígenes, métodos y encabezados
-				registry.addMapping("/")
-						.allowedOrigins("*") // Permitir todos los orígenes
-						.allowedMethods("*") // Permitir todos los métodos (GET, POST, etc.)
-						.allowedHeaders("*"); // Permitir todos los encabezados
-			}
-		};
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite CORS para todos los endpoints
+                .allowedOrigins("https://cinemapremier-venta-fxfrcegeb3b6gdb5.mexicocentral-01.azurewebsites.net")  // Reemplaza con la URL de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos HTTP permitidos
+                .allowedHeaders("*");  // Permite todos los encabezados
+    }
 }
+
